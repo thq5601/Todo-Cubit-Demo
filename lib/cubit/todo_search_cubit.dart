d0ea0d9eb62863230089b8d70e1279610c122ba0
QuanTH <thq5601@gmail.com>
2025-09-10 08:58:55 +0700

    Thêm file Cubit, sử dụng RxDart để debouce search

--- lib/cubit/todo_search_cubit.dart
+++ lib/cubit/todo_search_cubit.dart
@@ -6,6 +6,7 @@ class TodoSearchCubit extends Cubit<String> {
 
   TodoSearchCubit() : super('') {
     //Input --> Debounce --> emit
+    //Subscribe lắng nghe sự kiện từ searchSubject
     searchSubject.debounceTime(const Duration(microseconds: 300)).listen((
       query,
     ) {
@@ -13,12 +14,14 @@ class TodoSearchCubit extends Cubit<String> {
     });
   }
 
+  //Phương thức set query, những input từ searchBox sẽ được add vào searchSubject
   void setQuery(String query) {
     searchSubject.add(query);
   }
 
   @override
   Future<void> close() {
+    //Close để tránh memory leak
     searchSubject.close();
     return super.close();
   }
